ce013b5c8c86df7c07dde6f25a0a604f6003ce67
viktorhrv <viktor_swim@abv.bg>
2022-12-17 11:07:43 +0200

    Add simple logic for the game

--- src/first/repo/Game.java
+++ src/first/repo/Game.java
@@ -1,17 +1,80 @@
 package first.repo;
 
+import java.util.Scanner;
+
 public class Game {
+    private Deck deck;
+    private Player player;
+    private Dealer dealer;
+    
     public Game(){
-        Deck deck = new Deck();
-        Player player = new Player();
-        Dealer dealer = new Dealer();
+        deck = new Deck();
+        player = new Player();
+        dealer = new Dealer();
 
         deck.shuffle();
 
-        startGame();
+        playGame();
     }
 
-    private void startGame(){
-
+    private void playGame(){
+        dealer.getHand().takeCardFromDeck(deck);
+        dealer.getHand().takeCardFromDeck(deck);
+        
+        player.getHand().takeCardFromDeck(deck);
+        
+        System.out.println("Dealer's card: " + dealer.getHand().getCard(0));
+        System.out.println("Player's card: " + player.getHand().getCard(0));
+        
+        boolean playing = true;
+        
+        while(playing) {
+            System.out.println("Now, what do you want to do?");
+            System.out.println("T - Take another card");
+            System.out.println("S - Stop taking cards");
+            System.out.println("P - pass");
+            
+            Scanner sc = new Scanner(System.in);
+            char symbol = sc.next().charAt(0);
+            
+            if(symbol == 'T') {
+                 player.getHand().takeCardFromDeck(deck);
+                 System.out.println("New card: " + player.getHand().getCard(0));
+            }
+            else if(symbol == 'S') {
+                 playing = false;
+            }
+            else {
+                 System.out.println("Dealer wins!");
+                 return;
+            }
+        }
+        
+        if(player.getHand().calculateValue() > 21) {
+            System.out.println("Dealer wins!");
+            return;
+        }
+        
+        while(dealer.getHand().calculateValue() < 17) {
+            dealer.getHand().takeCardFromDeck(deck);
+        }
+        
+        if(dealer.getHand().calculateValue() > 21) {
+            System.out.println("Player wins!");
+            return;
+        }
+        
+        int playerHandValue = player.getHand().calculateValue();
+        int dealerHandValue = dealer.getHand().calculateValue();
+        
+        if(playerHandValue > dealerHandValue){
+            System.out.println("Player wins!");
+        }
+        else if(playerHandValue < dealerHandValue){
+            System.out.println("Dealer wins!");
+        }
+        else {
+            System.out.println("Tie!");
+        }
     }
 }
