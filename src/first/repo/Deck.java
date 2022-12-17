6104f5eef18db900d5c2083b6734831c64fbd39f
viktorhrv <viktor_swim@abv.bg>
2022-12-17 11:04:35 +0200

    Rename deck to cards

--- src/first/repo/Deck.java
+++ src/first/repo/Deck.java
@@ -6,26 +6,27 @@ import java.util.List;
 import java.util.Random;
 
 public class Deck {
-    private List<Card> deck;
+    private List<Card> cards;
     
     public Deck(){
-        deck = new ArrayList<Card>();
+        cards = new ArrayList<Card>();
         
         for(Rank rank : Rank.values()) {
             for (Suit suit : Suit.values()) {
                 Card newCard = new Card(rank, suit);
-                deck.add(newCard);
+                cards.add(newCard);
             }
         }
     }
 
     public void shuffle(){
-        Collections.shuffle(deck,new Random());
+        Collections.shuffle(cards,new Random());
     }
 
+    // Returns a COPY of the top card
     public Card takeCard(){
-        Card cardToTake = new Card(deck.get(0));
-        deck.remove(0);
+        Card cardToTake = new Card(cards.get(0));
+        cards.remove(0);
         return cardToTake;
     }
 }
