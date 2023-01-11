749775e69681fb944ba944beacf12ccce005dd33
Hadjiya <105456898+Hadjiya@users.noreply.github.com>
2023-01-12 00:18:37 +0200

    Add files via upload

---
+++ first-repo/src/Person.java
@@ -0,0 +1,17 @@
+//Parent class of Player and Dealer
+public abstract class Person {
+    private Hand hand;
+    
+    public Person() {
+        this.hand = new Hand();
+    }
+
+    public Hand getHand(){
+        return this.hand;
+    }
+    
+    public void setHand(Hand hand){
+        this.hand = hand;
+    }
+}
+    
