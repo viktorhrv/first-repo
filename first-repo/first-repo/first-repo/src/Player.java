74bd8952b11067a0d055dadb15e0816df3672bdf
Hadjiya <105456898+Hadjiya@users.noreply.github.com>
2023-01-11 13:30:03 +0200

    Add files via upload

---
+++ first-repo/first-repo/first-repo/src/Player.java
@@ -0,0 +1,15 @@
+
+public class Player extends Person{
+    private String name;
+    
+    public void set_name(String name){
+    this.name=name;
+    }
+    
+    public String get_Name(){
+        
+        return name;
+    }
+    
+    
+}
